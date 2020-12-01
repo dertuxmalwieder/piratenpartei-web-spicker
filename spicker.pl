512d8b6ff5c819cb06b97cbd9491cf20dfac395d
Cthulhux <Cthulhux@noemail.net>
2020-12-01 14:29:07 +0000

    Einrückung.
    FossilOrigin-Name: e60b14bacd27945f96b3a86119fa490e6ee78b581c439d9c01e8ed12e1c98c24

--- spicker.pl
+++ spicker.pl
@@ -17,84 +17,84 @@ our $APIURL = 'https://spickerrr.piraten-tools.de/api/';
 
 get '/' => sub {
     # Übersichtsseite:
-    my $c = shift;    
-    $c->render(template => 'start');
+  my $c = shift;    
+  $c->render(template => 'start');
 };
 
 post '/APIErrorHandler/:errorString' => sub {
-    # Fehlerbehandlung: Wirf HTML aus.
-    my $c = shift;
+  # Fehlerbehandlung: Wirf HTML aus.
+  my $c = shift;
     
-    $c->stash(APIURL => $APIURL);
-    $c->stash(errortext => $c->stash('errorString'));
+  $c->stash(APIURL => $APIURL);
+  $c->stash(errortext => $c->stash('errorString'));
     
-    $c->render(template => 'fehler_API');
+  $c->render(template => 'fehler_API');
 };
 
 post '/Parteitage' => sub {
-    # AJAX-Anfrage: Liste der Parteitage
-    my $c = shift;
+  # AJAX-Anfrage: Liste der Parteitage
+  my $c = shift;
     
-    my $ua  = Mojo::UserAgent->new;
-    my $url = "${APIURL}currentbooks";
+  my $ua  = Mojo::UserAgent->new;
+  my $url = "${APIURL}currentbooks";
 
-    $c->render(json => $ua->get($url)->result->json);
+  $c->render(json => $ua->get($url)->result->json);
 };
 
 post '/Parteitag/:parteitag' => sub {
-    # AJAX-Anfrage: Liste der Anträge
-    my $c = shift;
-    my $parteitag = $c->stash('parteitag');
+  # AJAX-Anfrage: Liste der Anträge
+  my $c = shift;
+  my $parteitag = $c->stash('parteitag');
     
-    if ($parteitag ne 'dummy') {    
-        my $ua  = Mojo::UserAgent->new;
-        my $url = "${APIURL}book/${parteitag}/motions";
+  if ($parteitag ne 'dummy') {    
+    my $ua  = Mojo::UserAgent->new;
+    my $url = "${APIURL}book/${parteitag}/motions";
         
-        $c->render(json => $ua->get($url)->result->json);
-    }
+    $c->render(json => $ua->get($url)->result->json);
+  }
 };
 
 post '/Parteitag/:parteitag/Antrag/:antrag' => sub {
-    # AJAX-Anfrage: Antrag :antrag für Parteitag :parteitag
-    my $c = shift;
-    my $parteitag = $c->stash('parteitag');
-    my $antrag = $c->stash('antrag');
+  # AJAX-Anfrage: Antrag :antrag für Parteitag :parteitag
+  my $c = shift;
+  my $parteitag = $c->stash('parteitag');
+  my $antrag = $c->stash('antrag');
     
-    if ($antrag ne 'dummy') {    
-        my $ua       = Mojo::UserAgent->new;
-        my $url      = "${APIURL}book/${parteitag}/motions";
-        my $antraege = $ua->get($url)->result->json;
+  if ($antrag ne 'dummy') {    
+    my $ua       = Mojo::UserAgent->new;
+    my $url      = "${APIURL}book/${parteitag}/motions";
+    my $antraege = $ua->get($url)->result->json;
         
-        my $needle_location = -1;
+    my $needle_location = -1;
         
-        while (my ($idx, $elem) = each(@{$antraege})) {
-            if ($elem->{'id'} eq $antrag) {
-                $needle_location = $idx;
-                last;
-            }
-        }
-        
-        # Der gesuchte Antrag steht jetzt in $antraege->[$needle_location].
-        # Aufbereiten für die Anzeige:
-        my $antrag = $antraege->[$needle_location];
-        
-        # Im Antragstext könnten Wikilinks drin sein. Bäh ... :-)
-        my $antragstext = $antrag->{text};
-        $antragstext =~ s/<a href="(\\)?\//<a href="https:\/\/wiki.piratenpartei.de\//g;
-        
-        # Der Link zum Autor ist vermutlich auch falsch.
-        my $author = $antrag->{author};
-        $author =~ s/<a href="(\/)?Benutzer:/<a target="_blank" href="https:\/\/wiki.piratenpartei.de\/Benutzer:/g;
-        
-        $c->content_for(author => $author);
-        $c->stash(origlink => $antrag->{url});
-        $c->stash(antragsid => $antrag->{id});
-        $c->stash(title => $antrag->{title});
-        $c->content_for(antrag => $antragstext);
-        $c->content_for(begruendung => $antrag->{remarks});
-        
-        $c->render(template => 'antrag');
+    while (my ($idx, $elem) = each(@{$antraege})) {
+      if ($elem->{'id'} eq $antrag) {
+        $needle_location = $idx;
+        last;
+      }
     }
+        
+    # Der gesuchte Antrag steht jetzt in $antraege->[$needle_location].
+    # Aufbereiten für die Anzeige:
+    my $antrag = $antraege->[$needle_location];
+        
+    # Im Antragstext könnten Wikilinks drin sein. Bäh ... :-)
+    my $antragstext = $antrag->{text};
+    $antragstext =~ s/<a href="(\\)?\//<a href="https:\/\/wiki.piratenpartei.de\//g;
+        
+    # Der Link zum Autor ist vermutlich auch falsch.
+    my $author = $antrag->{author};
+    $author =~ s/<a href="(\/)?Benutzer:/<a target="_blank" href="https:\/\/wiki.piratenpartei.de\/Benutzer:/g;
+        
+    $c->content_for(author => $author);
+    $c->stash(origlink => $antrag->{url});
+    $c->stash(antragsid => $antrag->{id});
+    $c->stash(title => $antrag->{title});
+    $c->content_for(antrag => $antragstext);
+    $c->content_for(begruendung => $antrag->{remarks});
+        
+    $c->render(template => 'antrag');
+  }
 };
 
 app->start;
